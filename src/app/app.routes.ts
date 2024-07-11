f5a948d49eb1db66a65bc5d18b1e5fcd10aa7f8f
filloax <filloax@gmail.com>
2024-07-12 01:43:53 +0200

    theming and material stuff

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
+    { path: '**', redirectTo: 'home'},
+    { path: 'home', component: HomeComponent },
     {
         path: 'old-site',
         loadComponent: () => import('./legacy/legacy.component').then(m => m.LegacyComponent)
