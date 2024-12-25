09276f34ddf995c666dc99ff82711957424f5590
filloax <filloax@gmail.com>
2024-12-25 01:26:30 +0100

    fix analogjs package, fix tsconfig paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,23 +4,20 @@ import mdx from '@astrojs/mdx';
 import remarkGfm from 'remark-gfm';
 import remarkSmartypants from 'remark-smartypants';
 import rehypeExternalLinks from 'rehype-external-links';
-import angular from '@analogjs/astro-angular';
 import remarkToc from 'remark-toc';
 import remarkRawFrontmatter from './plugins/remark-raw-frontmatter.mjs';
+
+import analogjsangular from '@analogjs/astro-angular';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://filloax.github.io',
   base: '/',
-  integrations: [
-    mdx(), 
-    svelte(), 
-    angular({
-      vite: {
-        inlineStylesExtension: 'scss|sass|less',
-      },
-    })
-  ],
+  integrations: [mdx(), svelte(), analogjsangular({
+    vite: {
+      inlineStylesExtension: 'scss|sass|less',
+    },
+  })],
   vite: {
     ssr: {
       // transform these packages during SSR. Globs supported
